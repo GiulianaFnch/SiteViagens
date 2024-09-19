1a582285f62b023edbdbd3dd18c89696918df9a8
giulianafnch <87533967+GiulianaFnch@users.noreply.github.com>
2024-09-19 16:21:49 +0100

    adicionei o listar/adicionar tours

---
+++ public/vendedor/vender_tours.php
@@ -0,0 +1,98 @@
+<?php
+include '../../config/valida.php';
+include '../../config/liga_bd.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Vender Tours</title>
+    <link href="style/dashboard.css" rel="stylesheet" type="text/css">
+    <script type="text/javascript">
+        function atualiza() {
+
+            document.getElementById("f2").elements.namedItem("valor_cat").value =
+                document.getElementById("f1").elements.namedItem("categoria").value;
+            document.getElementById("f2").elements.namedItem("valor_subcat").value =
+                document.getElementById("f1").elements.namedItem("subcategoria").value;
+        }
+        atualiza();
+
+    </script>
+</head>
+
+<body onload="atualiza();">
+    <h1 class="h2">Vender Passeioss</h1>
+    <?php
+
+    $categoria = 1;
+    /*
+    $categoria = $_POST['categoria'];
+    */
+    ?>
+
+    <form action="vender_tours.php" id="f1" method="post">
+        Categoria: <select name="categoria" id="categoria" onchange="this.form.submit();">
+            <?php
+            $sql = "SELECT * FROM t_categoria";
+            // a variavel resultado vai guardar todos os dados de todos os clientes
+            $resultado = mysqli_query($ligacao, $sql) or die(mysqli_error($ligacao));
+            //variavel para contar os registos
+            //enquanto conseguir ler dados do array resultado imprime
+            while ($linha = mysqli_fetch_assoc($resultado)) {
+                if ($categoria == $linha['id'])
+                /*if ($_POST['categoria'] == $linha['id'])*/
+                    echo "<option value='" . $linha['id'] . "' selected>" . $linha['categoria'] . "</option>";
+                else
+                    echo "<option value='" . $linha['id'] . "'>" . $linha['categoria'] . "</option>";
+            }
+            ?>
+        </select>
+        <br>Subcategoria: <select name="subcategoria" id="subcategoria" onchange="atualiza();">
+            <?php
+            $sql2 = "SELECT * FROM t_subcat where categoria=" . $categoria;
+            /*
+            $sql2 = "SELECT * FROM t_subcat where categoria=" . $_POST['categoria'];
+            */
+            // a variavel resultado vai guardar todas as subcategorias da categoria selecionada
+            
+            $resultado2 = mysqli_query($ligacao, $sql2) or die(mysqli_error($ligacao));
+            //enquanto conseguir ler dados do array resultado imprime
+            while ($linha2 = mysqli_fetch_assoc($resultado2)) {
+                echo "<option value='" . $linha2['id'] . "'>" . $linha2['subcat'] . "</option>";
+            }
+            ?>
+        </select>
+    </form>
+    <form action="vender_tours2.php" id="f2" method="post" enctype="multipart/form-data">
+        <input type="hidden" size="28" name="id_user" value="<?php echo $_SESSION['id']; ?>" required>
+        <input type="hidden" size="38" name="valor_cat" value="1" id="valor_cat" required>
+        <input type="hidden" size="30" name="valor_subcat" value="1" required>
+        Titulo:<input type="text" size="50" name="titulo" required><br /><br />
+        Descrição:<br>
+        <textarea cols="88" rows="5" name="descricao"></textarea><br /><br />
+        Preco:<input type="text" size="18" name="preco" required><br /><br />
+        Estado: <select name="estado">
+            <option value="1"> 1 estrela</option>
+            <option value="2"> 2 estrelas</option>
+            <option value="3"> 3 estrelas</option>
+            <option value="4"> 4 estrelas</option>
+            <option value="5"> 5 estrelas</option>
+        </select><br /><br />
+        Foto1: <input type="file" name="ficheiro1"><br><br>
+        Foto2:<input type="file" name="ficheiro2"><br><br>
+        Foto3:<input type="file" name="ficheiro3"><br><br>
+        <input type="submit" value="Vender">
+    </form>
+    <form action="vender.php" method="post">
+        <input type="hidden" name="categoria" value="1">
+        <input type="submit" value="Limpar">
+    </form>
+</body>
+
+</html>
+<?php
+//include_once "view/rodape.php";
+?>
---
+++ public/vendedor/vender_tours2.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <title>Horsed</title>
+</head>
+
+<body>
+    <h1>Registo de venda</h1>
+    <?php
+    include '../../config/liga_bd.php';
+
+    // Inicializar a variável $uploadOk
+    $uploadOk = 1;
+
+    // Verificar e validar a primeira foto
+    $_FILES["ficheiro"] = $_FILES["ficheiro1"];
+    include '../../config/valida_fotoa.php';
+
+    if ($uploadOk == 0) {
+        echo "O seu ficheiro não foi enviado.";
+    } else {
+        if ($uploadOk == 1) {
+            move_uploaded_file($_FILES["ficheiro"]["tmp_name"], $target_file);
+            //crio a instrução sql para adicionar um registo na base de dados
+            $sql = "INSERT INTO t_artigo (id_user, cat, subcat, titulo, descricao, preco, estado, foto1) VALUES ($_POST[id_user], $_POST[valor_cat], $_POST[valor_subcat], '$_POST[titulo]', '$_POST[descricao]', $_POST[preco], $_POST[estado], '" . $foto . "');";
+            // tento inserir na base de dados
+            //echo $sql;
+
+            if (mysqli_query($ligacao, $sql))
+                echo "<h2>Registo efetuado com sucesso! </h2>";
+
+            // Validação da foto 2
+            if (!empty($_FILES['ficheiro2']['name'])) {
+                $sql = "SELECT id FROM t_artigo ORDER BY id DESC LIMIT 1";
+                $resultado = mysqli_query($ligacao, $sql) or die(mysqli_error($ligacao));
+                $linha = mysqli_fetch_assoc($resultado);
+                $_FILES["ficheiro"] = $_FILES["ficheiro2"];
+                include '../../config/valida_fotoa.php';
+                if ($uploadOk == 1) {
+                    move_uploaded_file($_FILES["ficheiro"]["tmp_name"], $target_file);
+                    $sql2 = "UPDATE t_artigo SET foto2='" . $foto . "' WHERE id= $linha[id];";
+                    //echo $sql2;
+                    mysqli_query($ligacao, $sql2);
+                }
+            }
+
+            // Validação da foto 3
+            if (!empty($_FILES['ficheiro3']['name'])) {
+                $sql = "SELECT id FROM t_artigo ORDER BY id DESC LIMIT 1";
+                $resultado = mysqli_query($ligacao, $sql) or die(mysqli_error($ligacao));
+                $linha = mysqli_fetch_assoc($resultado);
+                $_FILES["ficheiro"] = $_FILES["ficheiro3"];
+                include '../../config/valida_fotoa.php';
+                if ($uploadOk == 1) {
+                    move_uploaded_file($_FILES["ficheiro"]["tmp_name"], $target_file);
+                    $sql3 = "UPDATE t_artigo SET foto3='" . $foto . "' WHERE id= $linha[id];";
+                    mysqli_query($ligacao, $sql3);
+                }
+            }
+        }
+    }
+
+    mysqli_close($ligacao);
+    ?>
+    <br />
+    <a href="login2.php" target="_self">Volta ao Menu</a>
+</body>
+
+</html>
