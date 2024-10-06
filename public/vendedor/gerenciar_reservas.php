a6708f5923460121d3ac22a54ddf33389eddd0b8
Guilherme Dourado Ferreira da Silva <guibezos2004@gmail.com>
2024-10-06 18:02:07 +0100

    Update gerenciar_reservas.php

--- public/vendedor/gerenciar_reservas.php
+++ public/vendedor/gerenciar_reservas.php
@@ -128,7 +128,7 @@ $result = $stmt->get_result();
                     <nav class="menu">
                         <a class="menu-item" style="color: #3A506B"><strong>Painel de Vendedor</strong></a>
                         <a class="menu-item" href="admin.php" onclick="showContent('profile');"><i class="bi bi-person-circle"></i> Editar Perfil</a>
-                        <a class="menu-item" href="vender_tours.php" onclick="showContent('vender-tours');"><i class="bi bi-bag"></i> Vender Tours</a>
+                        <a class="menu-item" href="vender_opcoes.php" onclick="showContent('vender-tours');"><i class="bi bi-bag"></i> Vender</a>
                         <a class="menu-item" href="gerenciar_reservas.php"><i class="bi bi-magic"></i> Gestão de Reservas</a>
                         <a class="menu-item" href="gestao_tours.php"><i class="bi bi-train-freight-front"></i> Gestão de Tours</a>      
                         <a class="menu-item" href="chat.php"><i class="bi bi-chat-dots"></i> Chat</a>
