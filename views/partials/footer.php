04f32ae6b8a2b306951528f6baa5ecb942f7229f
karoolinemoura <157537487+karoolinemoura@users.noreply.github.com>
2024-10-09 16:37:54 +0100

    Update footer.php

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,10 +1,50 @@
 <!--footer-->
 <section id="contact">
-    
-    <div class="end-text">
-        <p>© 2024 BestWay. Todos os direitos reservados.</p>
-    </div>
-</section>
+        <div class="footer">
+            <div class="main">
+                <div class="list">
 
+                    <h4> Minha Conta</h4>
+                    <ul>
+                    <li><a href="/SiteViagens/public/perfil.php">Meu Perfil</a></li>
+                        <li><a href="/SiteViagens/public/reservas.php">Minhas Viagens</a></li>
+                        <li><a href="/SiteViagens/public/backend/logout.php">Logout</a></li>
+                    </ul>
+                </div>
+
+                <div class="list">
+                    <h4>Suporte</h4>
+                    <ul>
+                        <li><a href="#">Contatos</a></li>
+                        <li><a href="#">Termos & Condições</a></li>
+                        <li><a href="#">Politica de privacidade</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="list">
+                    <h4>Trabalhe conosco</h4>
+                    <ul>
+                        <li><a href="public/vendedor/registro_vendedor.php">Como Parceiro Fornecedor</a></li>
+                        <li><a href="public/vendedor/admin.php">Acessar ao painel de vendedor</a></li>
+                    </ul>
+                </div>
+
+                <div class="list">
+                    <h4>Connect</h4>
+                    <div class="social">
+                        <a href="#"><i class='bx bxl-facebook'></i></a>
+                        <a href="#"><i class='bx bxl-instagram'></i></a>
+
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="end-text">
+            <p>© 2024 BestWay. Todos os direitos reservados.</p>
+        </div>
+    </section>
 <!--link to js-->
 <script type="text/javascript" src="../assets/js/script.js"></script>
