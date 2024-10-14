6876e2950f70d83cd0f1fdf941e8d09b7ddde39a
giulianafnch <87533967+GiulianaFnch@users.noreply.github.com>
2024-10-14 16:07:05 +0100

    ajuste header

--- views/partials/header.php
+++ views/partials/header.php
@@ -3,11 +3,11 @@
     <div class="bx bx-menu" id="menu-icon"></div>
 
     <ul class="navbar">
-            <li><a href="/SiteViagens/public/hotels/hotels.php">Hospedagem</a></li>
-            <li><a href="/SiteViagens/public/voos/voos.php">Passagens</a></li>
-            <li><a href="/SiteViagens/public/tours/tours.php">Passeios</a></li>
-            <li><a href="/SiteViagens/public/pacotes/pacotes.php">Pacotes</a></li>
-            <li><a href="/SiteViagens/public/perfil.php"><i class='bx bx-user'></i></a></li>
-            <li><a href="/SiteViagens/public/carrinho/carrinho.php"><i class='bx bx-cart'></i></a></li>
-        </ul>
+        <li><a href="/SiteViagens/public/hotels/hotels.php">Hospedagem</a></li>
+        <li><a href="/SiteViagens/public/voos/voos.php">Passagens</a></li>
+        <li><a href="/SiteViagens/public/tours/tours.php">Passeios</a></li>
+        <li><a href="/SiteViagens/public/pacotes/pacotes.php">Pacotes</a></li>
+        <li><a href="/SiteViagens/public/perfil.php"><i class='bx bx-user'></i></a></li>
+        <li><a href="/SiteViagens/public/carrinho/carrinho.php"><i class='bx bx-cart'></i></a></li>
+    </ul>
 </header>
